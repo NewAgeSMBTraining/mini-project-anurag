7f9f7b5d834054f4f45a7455cf35e4f443a07961
anu-newagesmb <anurag@newagesmb.com>
2021-10-28 12:43:13 +0530

    first commit

---
+++ Front-end/src/components/layout/Landing.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
+
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Employee Manegement System</h1>
+          <p className='lead'>
+         remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets contai The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsu
+          </p>
+          <div className='buttons'>
+            <Link to='/emplogin' className='btn btn-primary'>
+              Employee Login
+            </Link>
+            <Link to='/login' className='btn btn-light'>
+             Admin Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps)(Landing);
